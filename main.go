b4151674fc37b4c6558e7e79d6417d45e8185349
Delnegend <kiennguyen19323@gmail.com>
2024-05-21 02:20:42 +0700

    refactor(main.go): :recycle: cleanup env

--- main.go
+++ main.go
@@ -4,55 +4,31 @@ import (
 	"log"
 	"log/slog"
 	"os"
-	"strings"
 	"time"
 
-	get_update "social-2-telego/get_updates"
+	"social-2-telego/message_listener"
 	"social-2-telego/telegram"
+	"social-2-telego/utils"
 
 	"github.com/joho/godotenv"
 	"github.com/lmittmann/tint"
 )
 
-func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	token := os.Getenv("BOT_TOKEN")
-	if token == "" {
-		log.Fatal("BOT_TOKEN must be set")
-	}
-
-	// Colorful logging
+func init() {
 	slog.SetDefault(slog.New(
 		tint.NewHandler(os.Stderr, &tint.Options{
 			Level:      slog.LevelDebug,
 			TimeFormat: time.RFC1123Z,
 		}),
 	))
-
-	targetChannel := os.Getenv("CHANNEL")
-	if targetChannel == "" {
-		slog.Info("CHANNEL is not set, messages will be echoed back to the user.")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
 	}
+}
 
-	whitelistedAccountsEnv := os.Getenv("WHITELISTED")
-	whitelistedAccounts := strings.Split(whitelistedAccountsEnv, " ")
-	if len(whitelistedAccounts) == 0 {
-		slog.Info("WHITELISTED is not set, everyone can use the bot to send messages to your channel! You can add multiple usernames, @ not included, each separated by a space.")
-	}
+func main() {
+	appState := utils.InitAppState()
 
-	bot := telegram.Config{
-		BotToken:            token,
-		TargetChannel:       &targetChannel,
-		MessageQueue:        make(chan *telegram.Message),
-		WhitelistedAccounts: whitelistedAccounts,
-	}
-	go bot.Responder()
-
-	update_getter := get_update.Config{
-		BotToken:     token,
-		MessageQueue: &bot.MessageQueue,
-	}
-	update_getter.Initialize()
+	go telegram.InitResponder(appState)
+	message_listener.InitMessageListender(appState)
 }
