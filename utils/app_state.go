deb29982c3be24e1c532c9de54bc80968686a84a
Delnegend <kiennguyen19323@gmail.com>
2024-05-29 07:35:08 +0700

    refactor(utils): :recycle: update appstate initialization

--- utils/app_state.go
+++ utils/app_state.go
@@ -4,105 +4,171 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
+	"net/url"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type AppState struct {
-	botToken       string
-	targetChannel  string
-	numWorkers     int
-	allowedAccount []string
-	webhookDomain  string
-	port           string // for polling, ignore if use webhook
+	port string
+
+	useWebhook             bool
+	webhookDomain          string
+	webhookSecret          string
+	retrySetWebhookAttempt int
+
+	getUpdatesInterval time.Duration
+
+	botToken        string
+	artistDBDomain  string
+	allowedAccounts map[string]interface{}
+	targetChannel   string
+	numWorker       int
 
 	MsgQueue chan IncomingMessage
 }
 
-func InitAppState() *AppState {
-	as := &AppState{}
-	as.allowedAccount = make([]string, 0)
-	as.MsgQueue = make(chan IncomingMessage)
-
-	// parsing envs
-
-	numWorkers := os.Getenv("NUM_WORKERS")
-	if numWorkers == "" {
-		as.numWorkers = 5
-	} else {
-		num, err := strconv.Atoi(numWorkers)
-		if err != nil {
-			slog.Warn("NUM_WORKERS must be an integer, using default value 5")
-		}
-		as.numWorkers = num
+func NewAppState() *AppState {
+	appState := &AppState{
+		allowedAccounts: make(map[string]interface{}),
+		MsgQueue:        make(chan IncomingMessage),
 	}
 
-	as.botToken = os.Getenv("BOT_TOKEN")
-	if as.botToken == "" {
+	appState.port = os.Getenv("PORT")
+	if appState.port == "" {
+		slog.Warn("PORT is not set, defaulting to 8080")
+		appState.port = "8080"
+	}
+	portInt, err := strconv.Atoi(appState.port)
+	if err != nil {
+		slog.Warn("PORT must be an integer, defaulting to 8080")
+		appState.port = "8080"
+	}
+	if portInt < 0 || portInt > 65535 {
+		slog.Warn("PORT must be between 0 and 65535, defaulting to 8080")
+		appState.port = "8080"
+	}
+	appState.port = fmt.Sprintf("%d", portInt)
+
+	appState.useWebhook = strings.ToLower(os.Getenv("USE_WEBHOOK")) == "true"
+
+	appState.webhookDomain = os.Getenv("WEBHOOK_DOMAIN")
+	if _, err := url.ParseRequestURI(appState.webhookDomain); err != nil {
+		slog.Warn("WEBHOOK_DOMAIN is not a valid URL, webhook will not be enabled")
+		appState.useWebhook = false
+	}
+
+	webhookSecret := os.Getenv("WEBHOOK_SECRET")
+	if match, _ := regexp.MatchString(`[^a-zA-Z0-9_-]`, webhookSecret); match {
+		slog.Warn("WEBHOOK_SECRET must only contain alphanumeric characters, underscores, and hyphens, webhook will not be enabled")
+		appState.useWebhook = false
+	} else {
+		appState.webhookSecret = webhookSecret
+	}
+
+	appState.retrySetWebhookAttempt = 3
+	if appState.useWebhook {
+		retrySetWebhookAttempt := os.Getenv("RETRY_SET_WEBHOOK_ATTEMPT")
+		retrySetWebhookAttemptInt, err := strconv.Atoi(retrySetWebhookAttempt)
+		if err != nil {
+			slog.Warn("RETRY_SET_WEBHOOK_ATTEMPT must be an integer, defaulting to 3")
+		}
+		appState.retrySetWebhookAttempt = retrySetWebhookAttemptInt
+
+	}
+
+	appState.getUpdatesInterval = time.Second
+	if !appState.useWebhook {
+		getUpdatesInterval := os.Getenv("GET_UPDATES_INTERVAL")
+		getUpdatesIntervalDur, err := time.ParseDuration(getUpdatesInterval)
+		if err != nil {
+			slog.Warn("GET_UPDATES_INTERVAL is not a valid duration, defaulting to 1s")
+		}
+		appState.getUpdatesInterval = getUpdatesIntervalDur
+	}
+
+	appState.botToken = os.Getenv("BOT_TOKEN")
+	if appState.botToken == "" {
 		log.Fatal("BOT_TOKEN must be set")
 	}
 
-	as.targetChannel = os.Getenv("CHANNEL")
-	if as.targetChannel == "" {
-		slog.Info("CHANNEL is not set, messages will be echoed back to the user.")
+	appState.artistDBDomain = os.Getenv("ARTIST_DB_DOMAIN")
+	_, err = url.ParseRequestURI(appState.artistDBDomain)
+	if err != nil {
+		slog.Warn("ARTIST_DB_DOMAIN is not a valid URL, using the same social media as the given URL")
 	}
 
-	allowedAccount := os.Getenv("WHITELISTED")
-	if allowedAccount == "" {
-		slog.Info("WHITELISTED is not set, everyone can use the bot to send messages to your channel! You can add multiple usernames, @ is optional, each separated by a space.")
+	allowedAccounts := os.Getenv("ALLOWED_USERS")
+	if allowedAccounts == "" {
+		slog.Warn("ALLOWED_USERS is not set, everyone can use the bot to send messages to your channel! You can add multiple usernames, @ is optional, each separated by a space")
 	} else {
-		slice := strings.Split(allowedAccount, " ")
+		slice := strings.Split(allowedAccounts, ",")
 		for _, account := range slice {
 			account = strings.TrimPrefix(account, "@")
-			as.allowedAccount = append(as.allowedAccount, account)
+			appState.allowedAccounts[account] = struct{}{}
 		}
 	}
 
-	as.webhookDomain = os.Getenv("DOMAIN")
-	if as.webhookDomain == "" {
-		slog.Warn("DOMAIN is not set, webhook will not be enabled")
+	appState.targetChannel = os.Getenv("TARGET_CHANNEL")
+	if appState.targetChannel == "" {
+		slog.Info("TARGET_CHANNEL is not set, messages will be echoed back to the user")
+	}
+
+	numWorkers := os.Getenv("NUM_WORKERS")
+	if numWorkers == "" {
+		slog.Warn("NUM_WORKERS is not set, defaulting to 5")
+		appState.numWorker = 5
 	} else {
-		port := os.Getenv("PORT")
-		portInt, err := strconv.Atoi(port)
+		numWorkersInt, err := strconv.Atoi(numWorkers)
 		if err != nil {
-			slog.Warn("PORT is not set, defaulting to 8080")
-			portInt = 8080
+			slog.Warn("NUM_WORKERS must be an integer, defaulting to 5")
+			appState.numWorker = 5
+		} else {
+			appState.numWorker = numWorkersInt
 		}
-		if portInt < 0 || portInt > 65535 {
-			slog.Warn("PORT must be between 0 and 65535, defaulting to 8080")
-			portInt = 8080
-		}
-		as.port = fmt.Sprintf("%d", portInt)
-		slog.Info("listening on port", "port", as.port)
 	}
 
-	return as
+	return appState
 }
 
-func (as *AppState) BotToken() string {
-	return as.botToken
+func (c *AppState) GetPort() string {
+	return c.port
 }
-func (as *AppState) TargetChannel() string {
-	return as.targetChannel
+func (c *AppState) GetUseWebhook() bool {
+	return c.useWebhook
 }
-func (as *AppState) NumWorkers() int {
-	return as.numWorkers
+func (c *AppState) GetWebhookDomain() string {
+	return c.webhookDomain
 }
-func (as *AppState) IsAccountAllowed(account string) bool {
-	if len(as.allowedAccount) == 0 {
+func (c *AppState) GetWebhookSecret() string {
+	return c.webhookSecret
+}
+func (c *AppState) GetRetrySetWebhookAttempt() int {
+	return c.retrySetWebhookAttempt
+}
+func (c *AppState) GetGetUpdatesInterval() time.Duration {
+	return c.getUpdatesInterval
+}
+func (c *AppState) GetBotToken() string {
+	return c.botToken
+}
+func (c *AppState) GetArtistDBDomain() string {
+	return c.artistDBDomain
+}
+func (c *AppState) IsAccountAllowed(account string) bool {
+	if len(c.allowedAccounts) == 0 {
 		return true
 	}
-	for _, acc := range as.allowedAccount {
-		if acc == account {
-			return true
-		}
-	}
-	return false
+	account = strings.TrimPrefix(account, "@")
+	_, ok := c.allowedAccounts[account]
+	return ok
 }
-func (as *AppState) WebhookDomain() string {
-	return as.webhookDomain
+func (c *AppState) GetTargetChannel() string {
+	return c.targetChannel
 }
-func (as *AppState) Port() string {
-	return as.port
+func (c *AppState) GetNumWorkers() int {
+	return c.numWorker
 }
